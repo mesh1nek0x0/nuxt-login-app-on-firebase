c4c0d5720daaaa6d8c598d55e3c1b3900b364a63
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-10-11 18:36:24 +0900

    :wrench: toggle v-if with firebase authentication

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <section class="container">
-      <button @click="googleLogin">Googleでログイン</button>
+    <div v-if="isWaiting">
+      <p>読み込み中</p>
+    </div>
+    <div v-else>
+      <div v-if="!isLogin">
+        <button @click="googleLogin">Googleでログイン</button>
+      </div>
+      <div v-else>
+        <p>{{ user.email }}でログイン中</p>
+        <button @click="logOut">ログアウト</button>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,10 +19,32 @@
 import firebase from '@/plugins/firebase'
 
 export default {
+  asyncData () {
+    return {
+      isWaiting: true,
+      isLogin: false,
+      user: []
+    }
+  },
+  mounted: function () {
+    firebase.auth().onAuthStateChanged(user => {
+      this.isWaiting = false
+      if (user) {
+        this.isLogin = true
+        this.user = user
+      } else {
+        this.isLogin = false
+        this.user = []
+      }
+    })
+  },
   methods: {
     googleLogin () {
       const provider = new firebase.auth.GoogleAuthProvider()
       firebase.auth().signInWithRedirect(provider)
+    },
+    logOut () {
+      firebase.auth().signOut()
     }
   }
 }
