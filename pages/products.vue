95d8b4a057d2ce1c1740ba8e76e40c4ea3c4b2dd
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-10-14 22:28:50 +0900

    :wrench: add producs page with dummy

---
+++ pages/products.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>pages/products</h1>
+    <ul id="example-1">
+      <li v-for="product in products" v-bind:key="product.id">
+        {{ product.name }}
+        <ul>
+          <li>{{ product.price }}</li>
+          <li>{{ product.description }}</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      isWaiting: true,
+      isLogin: false,
+      products: []
+    };
+  },
+  mounted: function() {
+    this.products = [
+      {
+        id: "productId1",
+        name: "product1",
+        price: 2000,
+        description: "this is product 1"
+      },
+      {
+        id: "productId2",
+        name: "product2",
+        price: 3000,
+        description: "this is product 2"
+      }
+    ];
+  }
+};
+</script>
