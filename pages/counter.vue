38c50e8cf804419526f6305f3b24f700db546fd5
mesh1nek0x0 <mesh1nek0x0@gmail.com>
2019-10-22 17:25:47 +0900

    :wrench: add sample counter page

---
+++ pages/counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>pages/counter</h1>
+    <p>CURRENT: {{ counter }}</p>
+    <button type="submit" @click="handleIncrement">INC</button>
+    <button type="submit" @click="handleDecrement">DEC</button>
+    <hr />
+    <button type="submit" @click="handleReset">RESET</button>
+    <nuxt-link to="/other-counter">GO TO OTHERS</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    handleIncrement(event) {
+      this.counter++;
+    },
+    handleDecrement(event) {
+      this.counter--;
+    },
+    handleReset(event) {
+      this.counter = 0;
+    }
+  }
+};
+</script>
